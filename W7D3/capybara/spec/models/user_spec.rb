507c6db0e750f3e94e20991fd568935d4ca9a2c9
Will Bannister <bannister.will@gmail.com>
2022-11-19 01:52:02 -0800

    d

---
+++ W7D3/capybara/spec/models/user_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  subject(:user) do
+    User.create!(
+      email: "donuts@coffee.com",
+      password: "password"
+    )
+  end
+
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_length_of(:password).is_at_least(6) }
+
+  describe "#is_password?" do
+    it "verifies a password is correct" do
+      expect(user.is_password?("password")).to be true
+    end
+
+    it "verifies a password is not correct" do
+      expect(user.is_password?("bing_bong")).to be false
+    end
+  end
+
+  describe "#reset_session_token!" do
+    it "sets a new session token on the user" do
+      user.valid?
+      ol_toke = user.session_token
+      user.reset_session_token!
+
+      expect(user.session_token).to_not eq(ol_toke)
+    end
+
+    it "returns the new session token" do
+      expect(user.reset_session_token!).to eq(user.session_token)
+    end
+  end
+
+  describe "::find_by_credentials" do
+    before { user.save! }
+
+    it "returns user given good credentials" do
+      expect(User.find_by_credentials("donuts@coffee.com", "password")).to eq(user)
+    end
+
+    it "returns nil given bad credentials" do
+      expect(User.find_by_credentials("donuts@coffee.com", "bing_bong")).to eq(nil)
+    end
+  end
+end
---
+++ W7D3/capybara/spec/requests/users_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+
+  describe "GET /users/new (#new)" do
+    it "renders the new template" do
+      get new_user_url
+      expect(response.body).to include("Sign Up!")
+    end
+  end
+
+  describe "POST /users (#create)" do
+    context "with invalid params" do
+      it "validates the presence of the user's email" do
+        post users_url, params: {
+          user: {
+            password: "asdfasdf"
+          }
+        }
+        expect(response.body).to include("Sign Up!")
+        expect(CGI.unescapeHTML(response.body)).to include("Eail can't be blank")
+      end
+
+      it "validates that the password is at least 6 characters long" do
+        post users_url, params: {
+          user: {
+            username: "bad_bunni",
+            password: "asdfa"
+          }
+        }
+        expect(response.body).to include("Sign Up!")
+        expect(response.body).to include("Password is too short")
+      end
+    end
+
+    context "with valid params" do
+      it "redirects user to root page on success" do
+        post users_url, params: {
+          user: {
+            username: "bad_bunni",
+            password: "asdfa"
+          }
+        }
+        expect(response).to redirect_to(root_url)
+      end
+    end
+  end
+end
