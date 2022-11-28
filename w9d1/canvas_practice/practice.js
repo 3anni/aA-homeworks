4412e0941d5cbeb94d282d6f317501b5a6896e5f
Will Bannister <bannister.will@gmail.com>
2022-11-28 00:25:47 -0800

    completed w9d1 hw

--- w9d1/canvas_practice/practice.js
+++ w9d1/canvas_practice/practice.js
@@ -1,3 +1,24 @@
+
+
 document.addEventListener("DOMContentLoaded", function(){
+  const canvas = document.getElementById("mycanvas");
+  canvas.width = 500;
+  canvas.height = 500;
+
+  const c = canvas.getContext("2d");
+
+  c.fillStyle = "red";
+  c.fillRect(100, 100, 300, 300);
+
+  c.beginPath();
+  c.arc(250,250,80,0,2 * Math.PI,true);
+  c.strokeStyle = "white";
+  c.lineWidth = 20;
+  c.stroke();
+  c.fillStyle = "blue";
+  c.fill();
+
 
 });
+
+
